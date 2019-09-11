85653b389bf78a772396c104362df4858ccd69b7
bishops250 <biishops250@gmail.com>
2019-09-11 18:34:49 -0400

    Fixed Files Newest Version

--- src/Printing.java
+++ src/Printing.java
@@ -2,6 +2,7 @@
 public class Printing {
 
         public static void main(String[] args) {
+                System.out.println("What is the difference between\n a \' and a \"? Or between a \" and a \\\"?\n \n One is what we see when we're typing our program.\n The other is what appears on the \"console.\" ");
                 System.out.println("   \\/\n  \\\\//\n \\\\\\///\n ///\\\\\\\n  //\\\\\n   /\\\n");
         }
 }
