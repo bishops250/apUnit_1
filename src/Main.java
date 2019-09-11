4ac58b6985c4f25e74272ea92603df8268ac4b0d
bishops250 <biishops250@gmail.com>
2019-09-11 13:44:36 -0400

    Fixed Files Newest Version

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Math is fun!");
+        System.out.println("This is \"Aron's\" computer");
+        System.out.println("It is a great time\n to be a\n computer programmer!");
+    }
+}
