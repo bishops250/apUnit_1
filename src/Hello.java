cd10929ee5b849295edce462a6c0cd4fd1c6cd1e
bishops250 <bishops250@gmail.com>
2019-09-12 08:57:22 -0400

    Added to Hello.

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,8 @@
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("public class Hello {\n\tpublic stastic void main(String[] args) {\n " +
+                "\t\t System.out.println("Hello World!")\n\t}\n}");
     }
 }
