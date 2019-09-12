98d7ee5cc72ab3c3a6a8e772bbccb5a2bf2f500f
bishops250 <bishops250@gmail.com>
2019-09-12 09:01:07 -0400

    Fixed the Code I think

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("public class Hello {\n\tpublic stastic void main(String[] args) {\n " +
-                "\t\t System.out.println("Hello World!")\n\t}\n}");
+                "\t\t System.out.println(\"Hello World!\")\n\t}\n}");
     }
 }
