ff0daa534db9aa2e690bf0495989a7c5e57b1f46
bishops250 <bishops250@gmail.com>
2019-09-19 14:46:34 -0400

    added 1.4 warm ups

--- src/Round_Negatives.java
+++ src/Round_Negatives.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class Round_Negatives {
     public static void main(String[]args) {
-        double decimal_number=
+        Scanner user_input = new Scanner(System.in);
+
+
+
+        System.out.println("Please Input the number you would like rounded ");
+        double unrounded_number = user_input.nextDouble();
+
+        double rounded_number = (double) (unrounded_number + 0.5);
+
+        System.out.println(rounded_number);
     }
 }
