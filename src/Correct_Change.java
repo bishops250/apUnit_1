3cbd973b2ef4768ba44ef6d258eddffab8d8545d
bishops250 <biishops250@gmail.com>
2019-09-25 08:55:44 -0400

    Works but can be improved.

--- src/Correct_Change.java
+++ src/Correct_Change.java
@@ -3,24 +3,27 @@ public class Correct_Change {
 
     public static void main(String[]args) {
 
-        int  dollars, quarters, dimes, nickels, pennies, intial_amount_of_pennies;
+        int  dollars, quarters, dimes, nickels, pennies, intial_amount_of_pennies, remaining;
         Scanner user_input= new Scanner(System.in);
 
         System.out.println("Please enter the amount of pennies you have:");
         intial_amount_of_pennies = user_input.nextInt();
 
         //intial_amount_of_pennies = 25x + 10x + 5x + 1x;
+        remaining = intial_amount_of_pennies;
+        dollars = intial_amount_of_pennies / 100;
+        remaining-=dollars*100;
+        quarters = remaining/25;
+        remaining-=quarters*25;
+        dimes = remaining/10;
+        remaining-=dimes*10;
+        nickels = remaining/5;
+        remaining-=nickels*5;
+        pennies = remaining;
 
-        dollars=intial_amount_of_pennies / 100;
-        intial_amount_of_pennies %25 = quarters;
-        intial_amount_of_pennies %10 = dimes;
-        intial_amount_of_pennies %5 = nickels;
-        intial_amount_of_pennies = pennies;
 
-
-
-        System.out.println("The amount of pennies the user inputed amounts to" + dollars + "dollars" + quarters +
-                "quarters" + dimes + "dimes" + nickels + "nickels" + pennies + "pennies");
+        System.out.println(intial_amount_of_pennies+ " The amount of pennies the user inputed amounts to " + dollars + " dollars " + quarters +
+                " quarters " + dimes + " dimes " + nickels + " nickels " + pennies + " pennies ");
 
 
 
